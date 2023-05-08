857b04218ffe17d802d97a7c491e042c8953ada1
Armagdo <52860974+MostafaSaad1987@users.noreply.github.com>
2023-05-08 22:00:37 +0300

    JS Lesson 1.

---
+++ Foundation/JS/javascript.js
@@ -0,0 +1,16 @@
+console.log("Test");
+console.log(50 + 60);
+
+let x = 50;
+let y = 30;
+let z = 20;
+
+let a = x * 50;
+
+const MAX = 57;
+let actual = MAX - 13;
+let per = actual / MAX;
+console.log(per);
+
+console.log(x + y + z);
+console.log(a);
